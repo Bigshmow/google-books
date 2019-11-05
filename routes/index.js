9e81ac84c38b88e68dd5d25097f715c622b4fd68
Bigshmow <dstewart88@gmail.com>
2019-11-05 11:15:00 -0800

    routes updated and app runs in dev or prod mode based on scripts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-/* GET home page. */
+router.get('/api/health', function (req, res, next) {
+  res.json({
+    app: "project-3",
+    health: 200
+  });
+});
+
+// GET home page.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 module.exports = router;
