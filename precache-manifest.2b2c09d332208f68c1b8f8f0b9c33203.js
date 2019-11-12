ce7f7188d8ecb8c39f054ea08226e9a8685e2f86
Bigshmow <dstewart88@gmail.com>
2019-11-12 15:40:11 -0800

    Updates

---
+++ precache-manifest.2b2c09d332208f68c1b8f8f0b9c33203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c754f361206dcf23e425233e8bca385d",
+    "url": "/google-books/index.html"
+  },
+  {
+    "revision": "75fef4019f41ec2a9708",
+    "url": "/google-books/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "f6410d2c5f6a215731d3",
+    "url": "/google-books/static/css/main.6386c85c.chunk.css"
+  },
+  {
+    "revision": "75fef4019f41ec2a9708",
+    "url": "/google-books/static/js/2.2420b3a4.chunk.js"
+  },
+  {
+    "revision": "f6410d2c5f6a215731d3",
+    "url": "/google-books/static/js/main.4d1b480c.chunk.js"
+  },
+  {
+    "revision": "66d463cac340fdf5af24",
+    "url": "/google-books/static/js/runtime-main.c31b1978.js"
+  }
+]);
